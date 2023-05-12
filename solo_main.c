aa682de6a351f34d297ff2aeeb1a4bde8196d23e
2003solomon <solochidera10@gmail.com>
2023-05-12 09:55:28 +0100

    just a test file

---
+++ solo_main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int main(int ac, char **argv)
+{
+
+	while(1)
+	{
+	char *prompt ="myshell-> ";
+	char *lineptr;
+	size_t n = 0;
+
+// declaring void var
+	(void)ac; (void)argv;
+
+
+	printf("%s", prompt);
+	
+	getline(&lineptr, &n, stdin);
+	
+	printf("%s\n", lineptr);
+	
+	free(lineptr);
+	}
+
+	return (0);
+}
