e32c1a2e083605077a6db51830faf15ee0448364
2003solomon <solochidera10@gmail.com>
2023-05-15 15:26:38 +0100

    Wrote a program that passes betty check for task 0

---
+++ pass_betty.c
@@ -0,0 +1,13 @@
+#include"shell.h"
+/**
+ * main - Entry point of the programm
+ * printf: function that prints data to the stdout(standard output)
+ * Return: 0 on success and non-zero on error
+*/
+
+int main(void)
+{
+	printf("Am writing a program that passes the Betty check\n");
+
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,7 @@
+#ifndef SHELLS_H
+#define SHELLS_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#endif
