8848697360d3f8a72d47bf82efa4992f0da3010f
Muliro1 <mulirokhaemba@gmail.com>
2023-05-19 12:42:36 +0300

    ave

--- main.c
+++ main.c
@@ -6,38 +6,58 @@
 
 int main(int ac, char **argv){
   char *prompt = "($) ";
-  char *lineptr;
-  char *lineptr_copy = NULL;
+  char *command;
+  char *command_copy = NULL;
   const char *delim = " \n";
+  int token_count = 0;
+  char *token;
+  int i;
 
   size_t n = 0; 
   ssize_t nchars_read;
 
    /* declaring void variables */
-  (void)ac; (void)argv;
+  (void)ac;
 
     /* create an infinite loop */
     while (1){
       printf("%s", prompt);
-      nchars_read = getline(&lineptr, &n, stdin);
+      nchars_read = getline(&command, &n, stdin);
       /* check if the getline function failed or reached EOF or user use CTRL + D */ 
         if (nchars_read == -1){
             return (-1);
         }
-	lineptr_copy = malloc(sizeof(char) * nchars_read);
-	if (lineptr_copy== NULL)
+	command_copy = malloc(sizeof(char) * nchars_read);
+	if (command_copy == NULL)
 	{
 		        perror("memory allocation error");
 			return (-1);
 	}
-	copy_str(lineptr, lineptr_copy);
+	copy_str(command, command_copy);
+	token = strtok(command, delim);
 
-	printf("%s\n", lineptr);
-	printf("%s\n", lineptr_copy);
+	while (token != NULL)
+	{
+		token_count++;
+		token = strtok(NULL, delim);
+	}
+	token_count++;
+	argv = malloc(sizeof(char *) * token_count);
+	token = strtok(command_copy, delim);
+	for (i = 0; token != NULL; i++)
+	{
+		argv[i] = malloc(sizeof(char) * strlen(token));
+		copy_str(argv[i], token);
+		token = strtok(NULL, delim);
+	}
+	argv[i] = NULL;
+
+	printf("%s\n", command);
 
          
     }
-  free(lineptr); 
+    free(command_copy);
+    free(command);
 
   return (0);
 }
