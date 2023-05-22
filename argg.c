648a217a1df8a997cb0e5b4f7491935d745f1d95
2003solomon <solochidera10@gmail.com>
2023-05-22 13:31:48 +0100

    fixed a bug on argg.c

--- argg.c
+++ argg.c
@@ -1,4 +1,4 @@
-#include "shell.h"
+#include "main.h"
 
 
 int prompt(char **env)
--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define PROMPT "--$ "
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
